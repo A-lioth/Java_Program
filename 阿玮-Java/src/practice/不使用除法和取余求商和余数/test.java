090d6a1083eb3e0e40781f49e85e4e376a65bcfa
Alioth <3434039767@qq.com>
2024-07-20 16:34:19 +0800

    🌟 feat: 实现了不使用 % 和 / 求两个正整数的商和余数。

--- 阿玮-Java/src/practice/不使用除法和取余求商和余数/test.java
+++ 阿玮-Java/src/practice/不使用除法和取余求商和余数/test.java
@@ -1,7 +1,20 @@
 package practice.不使用除法和取余求商和余数;
 
 public class test {
+    // 给定两个正整数 m 和 n，求它们的商和余数。
+    public static void get_div_mod(int m, int n) {
+        int i = 0;
+        int sub = m - n;
+        while (sub > 0) {
+            sub -= n;
+            i++;
+        }
+        System.out.println("商：" + i + " 余数：" + (m - n * i));
+    }
+
     public static void main(String[] args) {
-        
+        int m = 10;
+        int n = 3;
+        get_div_mod(m, n);
     }
 }
