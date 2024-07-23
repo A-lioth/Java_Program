a2b76e2d9f5e37033925bb33974d05e51c08281c
Alioth <3434039767@qq.com>
2024-07-23 15:25:35 +0800

    ✨ feat: 列举了一些三目运算符代替选择结构的例子。

---
+++ 阿玮-Java/src/practice/三目运算符代替选择结构/test.java
@@ -0,0 +1,25 @@
+package practice.三目运算符代替选择结构;
+
+public class test {
+    public static void main(String[] args) {
+        /*
+        cost = (int)((price < 100) ? price : 
+                     (price < 500) ? price * 0.9 : 
+                     (price < 2000) ? price * 0.8 : 
+                     (price < 5000) ? price * 0.7 : price * 0.6);
+
+        System.out.println(cost);
+        */
+        /*
+        System.out.println(bmi < 18.5 ? "偏瘦" : 
+                        bmi < 20.9 ? "苗条" : 
+                        bmi < 24.9 ? "适中" : "偏胖");
+        */
+        /* 
+        System.out.println("A".equals(grade) ? "优秀" : 
+                           "B".equals(grade) ? "良好" : 
+                           "C".equals(grade) ? "及格" : 
+                           "D".equals(grade) ? "不及格" : "未知等级"); 
+        */
+    }
+}
