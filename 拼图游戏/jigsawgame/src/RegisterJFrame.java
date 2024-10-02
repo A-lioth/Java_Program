0409ab622f796f264ca3e041ce055c2dc4af7197
Alioth <3434039767@qq.com>
2024-10-02 10:40:19 +0800

    🎉 init: 创建了拼图游戏登录、注册和主界面窗口，实现了基本的窗口功能和应用启动逻辑。

---
+++ 拼图游戏/jigsawgame/src/RegisterJFrame.java
@@ -0,0 +1,21 @@
+package 拼图游戏.jigsawgame.src;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame() {
+        // * 设置窗口大小
+        this.setSize(488, 500);
+        // * 设置窗口标题
+        this.setTitle("拼图游戏-注册");
+        // * 设置窗口居中
+        this.setLocationRelativeTo(null);
+        // * 设置窗口置顶
+        this.setAlwaysOnTop(true);
+        // * 设置窗口关闭方式
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+}
---
+++ 拼图游戏/puzzlegame/src/App.java
@@ -0,0 +1,13 @@
+import 拼图游戏.jigsawgame.src.GameJFrame;
+import 拼图游戏.jigsawgame.src.LoginJFrame;
+import 拼图游戏.jigsawgame.src.RegisterJFrame;
+
+public class App {
+    public static void main(String[] args) {
+        new LoginJFrame();
+
+        new RegisterJFrame();
+
+        new GameJFrame();
+    }
+}
