47c722ad6e3060ab7a3cff094afeb006820fd998
Alioth <3434039767@qq.com>
2024-10-02 17:22:28 +0800

    ✨ feat: 重构代码结构，重命名文件及包名，优化代码可读性。

---
+++ puzzlegame/src/cn/edu/nmu/App.java
@@ -0,0 +1,11 @@
+package cn.edu.nmu;
+
+public class App {
+    public static void main(String[] args) {
+        new LoginJFrame();
+
+        new RegisterJFrame();
+
+        new GameJFrame();
+    }
+}
